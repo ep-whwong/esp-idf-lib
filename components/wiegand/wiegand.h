975c3d9e8070ff1f8cf7aacfa9b633e1ac5f1857
ep-whwong <wh.wong@entrypass.net>
2025-02-12 14:55:44 +0800

    WIP 1. To enable compilation after cleaning up the definition.

--- components/wiegand/wiegand.h
+++ components/wiegand/wiegand.h
@@ -43,6 +43,8 @@
 #include <esp_err.h>
 #include <esp_timer.h>
 
+#include "../../../../EdgeV2/main/common_defines.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -90,8 +92,8 @@ struct wiegand_reader
  * @param byte_order       Byte order of data
  * @return `ESP_OK` on success
  */
-esp_err_t wiegand_reader_init(wiegand_reader_t *reader[CONFIG_NUM_SUPPORTED_READERS_PER_PD],
-    gpio_num_t gpio_d0[CONFIG_NUM_SUPPORTED_READERS_PER_PD], gpio_num_t gpio_d1[CONFIG_NUM_SUPPORTED_READERS_PER_PD],
+esp_err_t wiegand_reader_init(wiegand_reader_t *reader[DEF_NUM_SUPPORTED_READERS_PER_PD],
+    gpio_num_t gpio_d0[DEF_NUM_SUPPORTED_READERS_PER_PD], gpio_num_t gpio_d1[DEF_NUM_SUPPORTED_READERS_PER_PD],
     bool internal_pullups, size_t buf_size, wiegand_callback_t callback, wiegand_order_t bit_order,
     wiegand_order_t byte_order, uint8_t num_wie_rd);
 
